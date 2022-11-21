d70a19baaa75b9e3ba387b1eae6d2ea9235cf391
poramental <poramental@mail.ru>
2022-11-21 22:43:21 +0300

    1 задание

--- src/main/java/com/javalab3/lab3/lab/z1/ResponseAfterSort.java
+++ src/main/java/com/javalab3/lab3/lab/z1/ResponseAfterSort.java
@@ -14,6 +14,6 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ResponseAfterSort {
     
-    List<String> byLenth;
+    List<String> byLength;
     List<String> byCharCounts;
 }
--- src/main/java/com/javalab3/lab3/lab/z1/Z1Controller.java
+++ src/main/java/com/javalab3/lab3/lab/z1/Z1Controller.java
@@ -2,10 +2,13 @@ package com.javalab3.lab3.lab.z1;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -24,13 +27,27 @@ public class Z1Controller {
         return "z1";
     }
 
-    @PutMapping(value="/z1")
+    @PostMapping(value="/z1")
     public String Z1post(@RequestParam("reqstring") String string, Model model) {
-        RequestList reqList = new RequestList();
-        model.addAttribute("requestList", reqList);
-
-        return "redirect:/z1Resp";
+        // RequestList reqList = new RequestList();
+        // model.addAttribute("requestList", reqList);
+        
+        String[] _list = string.split("\n");
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < _list.length; i++ ){
+            list.add(_list[i]);
+        }
+        ResponseAfterSort resp = new ResponseAfterSort();
+        resp.setByCharCounts(StringListSorter.sortByCountsOfLowerChars(list));
+        resp.setByLength(StringListSorter.sortByLength(list));
+        model.addAttribute("resp", resp);
+        return "z1Resp";
     }
     
+
+    @GetMapping(value = "/z1Resp")
+    public String z1Resp(){
+        return "z1Resp";
+    }
     
 }
